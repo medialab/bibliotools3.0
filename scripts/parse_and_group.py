5329c3e974ebcf2d930ff57a9d2d9f542a519042
Paul Girard <paul.girard@sciences-po.fr>
2014-12-01 14:23:08 +0100

    added centralized conf

--- scripts/parse_and_group.py
+++ scripts/parse_and_group.py
@@ -1,24 +1,18 @@
 import os
 from parser import Wos_parser
-
-indir="/home/pgi/Documents/events/20141125_sprint_medea/"
-outdir_prefix= "/home/pgi/Documents/events/20141125_sprint_medea/bibtools-data-parsed"
-one_saverec_file="all_years.txt"
-wos_headers="PT	AU	BA	BE	GP	AF	BF	CA	TI	SO	SE	BS	LA	DT	CT	CY	CL	SP	HO	DE	ID	AB	C1	RP	EM	RI	OI	FU	FX	CR	NR	TC	Z9	PU	PI	PA	SN	EI	BN	J9	JI	PD	PY	VL	IS	PN	SU	SI	MA	BP	EP	AR	DI	D2	PG	WC	SC	GA	UT	PM"
-
-years_spans={
-	"pre-AR1":[1970,1989],
-	"pre-AR2":[1990,1994],
-	"pre-AR3":[1995,2000],
-	"pre-AR4":[2001,2006],
-	"pre-AR5":[2007,2013]
-}
+from config import CONFIG
 
 
-output_dir="by_AR"
+indir=os.path.dirname(CONFIG["one_file_corpus"])
+output_dir="wosdata_by_AR"
 if not os.path.exists(os.path.join(indir,output_dir)):
 	os.mkdir(os.path.join(indir,output_dir))
-	
+outdir_prefix=CONFIG["parsed_data"]
+if not os.path.exists(outdir_prefix):
+	os.mkdir(outdir_prefix)
+
+years_spans=dict((s,data["years"]) for s,data in CONFIG["spans"].iteritems())
+
 files={}
 for (l,ys) in years_spans.iteritems():
 	if not os.path.exists(os.path.join(indir,output_dir,l)):
@@ -26,20 +20,18 @@ for (l,ys) in years_spans.iteritems():
 	if os.path.exists(os.path.join(indir,output_dir,l,l)+".txt"):
 		os.remove(os.path.join(indir,output_dir,l,l)+".txt")
 	files[l]=open(os.path.join(indir,output_dir,l,l)+".txt","w")
-	files[l].write(wos_headers+"\n")
+	files[l].write(CONFIG["wos_headers"]+"\n")
 
-onefile_output=open(os.path.join(indir,one_saverec_file),"r")
+onefile_output=open(CONFIG["one_file_corpus"],"r")
 onefile_output.readline()
 for line in onefile_output.readlines():
 	# filter blank lines
 	if "\t" in line:
 		try:
 			y=int(line.split("\t")[42])
-			find=0
 			for (l,ys) in years_spans.iteritems():
 				if y >= ys[0] and y<= ys[1]:
 					files[l].write(line)
-					find=1
 		
 		except : 
 			print "oups year %s can't be cat to int "%y
